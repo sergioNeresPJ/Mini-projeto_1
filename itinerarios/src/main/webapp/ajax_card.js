e6201ab5f3d7f6332cc2127bb15d8af79ac4400e
Dival Siqueira <dival.neto@estudante.ufscar.br>
2023-12-10 18:45:07 -0300

    Esta dispondo os cards na tela :)

--- itinerarios/src/main/webapp/ajax_card.js
+++ itinerarios/src/main/webapp/ajax_card.js
@@ -5,10 +5,14 @@ function initAjax() {
 function getItinerarios() {
   const xhttp = initAjax();
 
-  xhttp.onload = function() {
-    console.log("estou aqui")
-    xmlCard(this)
-  }
+  xhttp.onload = function () {
+    console.log("XML carregado com sucesso.");
+    xmlCard(this);
+  };
+
+  xhttp.onerror = function () {
+    console.error("Erro ao carregar o XML.");
+  };
 
   xhttp.open("GET", "itinerarios.xml");
   xhttp.send();
@@ -16,12 +20,50 @@ function getItinerarios() {
 
 function xmlCard(xml) {
   const xmlDoc = xml.responseXML;
-  console.log(xml)
-  console.log(xmlDoc)
   const data = xmlDoc.getElementsByTagName("ITINERARIO");
-  console.log(data);
-  // aqui vao os cards
+
+  const elementoHTML = document.querySelector("#conteudo-dinamico");
+  console.log(elementoHTML)
+
+  // Criar uma string HTML para armazenar os cards
+  let htmlString = "";
+
+  for (let i = 0; i < Math.min(data.length, 8); i++) {
+    const nome = data[i].getElementsByTagName("NOME")[0].childNodes[0].nodeValue;
+    const regiao = data[i].getElementsByTagName("REGIAO")[0].childNodes[0].nodeValue;
+
+    // Obter todas as paradas do itinerário
+    const paradasNodes = data[i].getElementsByTagName("PARADA");
+
+    let paradas = "";
+
+    // Iterar sobre todas as paradas do itinerário
+    for (let j = 0; j < paradasNodes.length; j++) {
+      const parada = paradasNodes[j].childNodes[0].nodeValue;
+      paradas += `${parada}<br>`;
+    }
+
+    const card = `
+    <div class="card col">
+      <div class="card-body">
+        <h5 class="card-title">${nome}</h5>
+        <p class="card-text">Região: ${regiao} <br> Paradas: ${paradas}</p>
+      </div>
+    </div>
+  `;
+
+
+    // Adicionar o HTML do card à string
+    htmlString += card;
+  }
+
+  // Atribuir a string HTML ao conteúdo do elemento
+  elementoHTML.innerHTML = htmlString;
+
+  console.log("Cards adicionados com sucesso!");
 }
+
+
 
 function postItinerario() {
   ajax = initAjax();
@@ -50,7 +92,7 @@ function postItinerario() {
       else console.log("Não foi! :C", ajax.readyState, ajax.status)
     }
 
-    url = "/itinerarios/itinerarioservletajax?"+data;
+    url = "/itinerarios/itinerarioservletajax?" + data;
     ajax.open("GET", url, true);
     ajax.send(null);
   }
--- itinerarios/target/itinerarios/index.jsp
+++ itinerarios/target/itinerarios/index.jsp
@@ -3,28 +3,30 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>itinerários</title>
+    <link rel="stylesheet" href="../styles/home.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div id="header-placeholder"></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>
+      $(document).ready(function () {
+        $("#header-placeholder").load("./header/header.jsp");
+      });
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>itinerários</title>
-        <link rel="stylesheet" href="../styles/home.css">
-        
-    </head>
-    <body>
+      document.addEventListener("DOMContentLoaded", function () {
+        getItinerarios();
+      });
+    </script>
 
-        <div id="header-placeholder"></div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $("#header-placeholder").load("./header/header.jsp");
-            });
+    <div id="conteudo-dinamico" class="row row-cols-2">
+      <!-- O conteúdo dinâmico vai aqui -->
+    </div>
 
-            
-        </script>
-
-        <button onclick="getItinerarios()">GetItinerarios</button>
-
-    </body>
     <script src="./ajax_card.js"></script>
+  </body>
 </html>
